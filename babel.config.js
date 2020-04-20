b52926b93a26e0300bbc3cf8d46567896996f832
aredra <rnswn48@gmail.com>
2020-04-20 20:11:05 +0900

    Added Babel plugins

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    presets: [
+        './my-babel-preset.js'
+    ]
+};
---
+++ my-babel-plugin.js
@@ -0,0 +1,25 @@
+module.exports = function myBabelPlugin() {
+    return {
+        visitor: {
+            Identifier(path) {
+                const name = path.node.name;
+
+                // 바벨이 만든 AST 노드를 출력
+                console.log('Identifier() name: ', name);
+
+                // 변환작업: 코드 문자열을 역순으로 변환
+                path.node.name = name
+                    .split("")
+                    .reverse()
+                    .join("");
+            },
+            VariableDeclaration(path) {
+                console.log('VariableDelaration() kind: ', path.node.kind);
+                
+                if (path.node.kind === 'const') {
+                    path.node.kind = 'var';
+                }
+            }
+        }
+    }
+}
